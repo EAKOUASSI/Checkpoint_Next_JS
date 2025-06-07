e681c01075eef7426ce9068e7d6cb5442b1343ec
EAKOUASSI <eak.kouassi@gmail.com>
2025-06-07 00:17:17 +0000

    Ajout de l'image de la bannière + correction HeroSection

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -3,7 +3,7 @@ export default function HeroSection() {
     <section
       id="accueil"
       className="h-screen bg-cover bg-center text-white text-center p-8"
-      style={{ backgroundImage: "url('/images/Image_kea.jpg')" }}
+      style={{ backgroundImage: "url('/Images/image_kea.jpg')" }}
     >
       <h2 className="text-4xl font-bold">Bienvenue sur mon portfolio</h2>
         <p className="mt-4">
